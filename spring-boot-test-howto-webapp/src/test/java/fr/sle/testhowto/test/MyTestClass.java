8e575634ec13a1255bdb38946a30d0d782cf8245
slem1 <sylvain.lem1@gmail.com>
2017-10-17 13:53:49 +0200

    Add first test

---
+++ spring-boot-test-howto-webapp/src/test/java/fr/sle/testhowto/test/MyTestClass.java
@@ -0,0 +1,20 @@
+package fr.sle.testhowto.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author slemoine
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyTestClass {
+
+    @Test
+    public void aTestShouldAssertSomething(){
+
+    }
+
+}
