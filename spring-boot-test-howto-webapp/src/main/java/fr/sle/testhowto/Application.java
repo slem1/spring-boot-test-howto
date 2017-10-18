191eee5de709dd0a6b464662fb348f023cada832
slem1 <sylvain.lem1@gmail.com>
2017-10-18 11:44:04 +0200

    Add application entry point

---
+++ spring-boot-test-howto-webapp/src/main/java/fr/sle/testhowto/Application.java
@@ -0,0 +1,15 @@
+package fr.sle.testhowto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author slemoine
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String... args) {
+        SpringApplication.run(Application.class);
+    }
+}
